6ea0244807cbb7403cad2aa338a30c70cc7a6e6f
Simon <zarok13>
2025-08-01 10:10:54 +0400

    fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
 set -e fish_user_paths
 set -U fish_user_paths $HOME/.bin  $HOME/.local/bin /var/lib/flatpak/exports/bin/ $fish_user_paths
 
-
 set fish_greeting
 
+set -g __fish_git_autostatus 0
+set -U fish_color_command brgreen
 if status is-interactive
 end
 
